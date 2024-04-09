98a57f27a4b5e0dc93dd59c1b3ea01ac9da8d594
aviationguy24 <161404050+aviationguy24@users.noreply.github.com>
2024-04-09 16:18:57 -0700

    Update source.c

--- Assignment/Binary_Calc/source.c
+++ Assignment/Binary_Calc/source.c
@@ -22,7 +22,58 @@
  *      V2.1: Updated with fixed operator function
  *      V2.2: Updated Output and Input ports
  *      V2.3: Updated Output and Input ports
+ *      V2.4: Updated with Pragma Headers
  */
+
+// CONFIG1L
+#pragma config FEXTOSC = LP     // External Oscillator Selection (LP (crystal oscillator) optimized for 32.768 kHz; PFM set to low power)
+#pragma config RSTOSC = EXTOSC  // Reset Oscillator Selection (EXTOSC operating per FEXTOSC bits (device manufacturing default))
+
+// CONFIG1H
+#pragma config CLKOUTEN = OFF   // Clock out Enable bit (CLKOUT function is disabled)
+#pragma config PR1WAY = ON      // PRLOCKED One-Way Set Enable bit (PRLOCK bit can be cleared and set only once)
+#pragma config CSWEN = ON       // Clock Switch Enable bit (Writing to NOSC and NDIV is allowed)
+#pragma config FCMEN = ON       // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor enabled)
+
+// CONFIG2L
+#pragma config MCLRE = EXTMCLR  // MCLR Enable bit (If LVP = 0, MCLR pin is MCLR; If LVP = 1, RE3 pin function is MCLR )
+#pragma config PWRTS = PWRT_OFF // Power-up timer selection bits (PWRT is disabled)
+#pragma config MVECEN = ON      // Multi-vector enable bit (Multi-vector enabled, Vector table used for interrupts)
+#pragma config IVT1WAY = ON     // IVTLOCK bit One-way set enable bit (IVTLOCK bit can be cleared and set only once)
+#pragma config LPBOREN = OFF    // Low Power BOR Enable bit (ULPBOR disabled)
+#pragma config BOREN = SBORDIS  // Brown-out Reset Enable bits (Brown-out Reset enabled , SBOREN bit is ignored)
+
+// CONFIG2H
+#pragma config BORV = VBOR_2P45 // Brown-out Reset Voltage Selection bits (Brown-out Reset Voltage (VBOR) set to 2.45V)
+#pragma config ZCD = OFF        // ZCD Disable bit (ZCD disabled. ZCD can be enabled by setting the ZCDSEN bit of ZCDCON)
+#pragma config PPS1WAY = ON     // PPSLOCK bit One-Way Set Enable bit (PPSLOCK bit can be cleared and set only once; PPS registers remain locked after one clear/set cycle)
+#pragma config STVREN = ON      // Stack Full/Underflow Reset Enable bit (Stack full/underflow will cause Reset)
+#pragma config DEBUG = OFF      // Debugger Enable bit (Background debugger disabled)
+#pragma config XINST = OFF      // Extended Instruction Set Enable bit (Extended Instruction Set and Indexed Addressing Mode disabled)
+
+// CONFIG3L
+#pragma config WDTCPS = WDTCPS_31// WDT Period selection bits (Divider ratio 1:65536; software control of WDTPS)
+#pragma config WDTE = OFF       // WDT operating mode (WDT Disabled; SWDTEN is ignored)
+
+// CONFIG3H
+#pragma config WDTCWS = WDTCWS_7// WDT Window Select bits (window always open (100%); software control; keyed access not required)
+#pragma config WDTCCS = SC      // WDT input clock selector (Software Control)
+
+// CONFIG4L
+#pragma config BBSIZE = BBSIZE_512// Boot Block Size selection bits (Boot Block size is 512 words)
+#pragma config BBEN = OFF       // Boot Block enable bit (Boot block disabled)
+#pragma config SAFEN = OFF      // Storage Area Flash enable bit (SAF disabled)
+#pragma config WRTAPP = OFF     // Application Block write protection bit (Application Block not write protected)
+
+// CONFIG4H
+#pragma config WRTB = OFF       // Boot Block Write Protection bit (Boot Block not write-protected)
+#pragma config WRTC = OFF       // Configuration Register Write Protection bit (Configuration registers not write-protected)
+#pragma config WRTD = OFF       // Data EEPROM Write Protection bit (Data EEPROM not write-protected)
+#pragma config WRTSAF = OFF     // SAF Write protection bit (SAF not Write Protected)
+#pragma config LVP = ON         // Low Voltage Programming Enable bit (Low voltage programming enabled. MCLR/VPP pin function is MCLR. MCLRE configuration bit is ignored)
+
+// CONFIG5L
+#pragma config CP = OFF         // PFM and Data EEPROM Code Protection bit (PFM and Data EEPROM code protection disabled)
 
 
 #include <xc.h> 
@@ -30,6 +81,7 @@
 
 
 #define _XTAL_FREQ 4000000 /*Clock Frequency of 4Mhz */
+#define FCY    _XTAL_FREQ/4
 
 
 /* Declarations or Initializations*/
@@ -47,6 +99,8 @@ int Division(int, int); /* Division Operation */
 int Addition(int, int); /* Addition Operation */
 int Subtraction(int, int); /* Subtraction Operation */
 /* -------------------  */
+
+
 
 void main(void) {
    Operation_Set = 0;   /* The register to store the operation
@@ -95,19 +149,8 @@ void main(void) {
             X_Input_REG = Keypad_Input(X_Input_REG);
             if (X_Input_REG > 0){
                 PORTD = 1;
-                Program_delay(100,100);
+                //Program_delay(100,100);
             }
-        }
-        if (PORTD > 0){ 
-            PORTBbits.RB1 = 1;
-            if (PORTBbits.RB6 == 1)
-            {
-                PORTD = 0;
-                X_Input_REG = 0;
-                Y_Input_REG = 0;
-                Operation_REG = 0;
-            }
-            PORTBbits.RB1 = 0;
         }
         
         
@@ -120,7 +163,7 @@ void main(void) {
         if (Operation_Set > 0){
             PORTD = 3;
             Operation_Set = 0;
-            Program_delay(100,100);
+            //Program_delay(100,100);
         }
         
         //Checks for the Second User Input from Keypad
@@ -129,7 +172,7 @@ void main(void) {
                                             //input to Y input
             if (Y_Input_REG > 0){ //check if it's a valid input
                 PORTD = 2; //if so, set PortD = 2
-                Program_delay(100,100);
+               // Program_delay(100,100);
             }
             if (PORTD > 0){
                 PORTBbits.RB1 = 1;  
@@ -168,7 +211,7 @@ void main(void) {
                 PORTBbits.RB2 = 1;
                 if (PORTBbits.RB6 == 1){
                     PORTD = Display_Result_REG; //outputs the results
-                    Program_delay(100,100);
+                    //Program_delay(100,100);
                 }
                 PORTBbits.RB2 = 0;
             }
@@ -194,14 +237,29 @@ void main(void) {
 
 
 // Delay function of the Program
-void Program_delay(unsigned int inner, unsigned int outer)
+/*void Program_delay(unsigned int inner, unsigned int outer)
 {
     
     unsigned int i; unsigned int j;
     for (i = 0; i <inner ;  i++) //Inner LOOP
         for (j = 0 ; j<outer ; j++); //Outer LOOP
 }    
+*/
+// Multiplication Function
+int Multiplication (int X3, int Y3)
+{
+    int Multiply;
+    Multiply = X3 * Y3;
+    return Multiply;
+}
 
+// Division Function
+int Division (int X4, int Y4)
+{
+    int Divide;
+    Divide = X4 / Y4;
+    return Divide;
+}
 
 // Addition Function
 int Addition (int X1, int Y1)
@@ -219,21 +277,6 @@ int Subtraction (int X2, int Y2)
     return Subtract;
 }
 
-// Multiplication Function
-int Multiplication (int X3, int Y3)
-{
-    int Multiply;
-    Multiply = X3 * Y3;
-    return Multiply;
-}
-
-// Division Function
-int Division (int X4, int Y4)
-{
-    int Divide;
-    Divide = X4 / Y4;
-    return Divide;
-}
 
 // Operation check function
 int Operations_check(int what_op){
